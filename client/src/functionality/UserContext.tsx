0f6d5230faeda675f55be42f23bbb5c3517b1f0d
Sofia Katzman <katzmansof@gmail.com>
2023-08-31 01:14:17 -0400

    comment out

--- client/src/functionality/UserContext.tsx
+++ client/src/functionality/UserContext.tsx
@@ -7,36 +7,25 @@ export function UserProvider({ children }: { children: ReactNode }) {
 
   const [user, setUser] = useState<UserType | null>(null)
   
-  useEffect(() => {
-    // Sample data Placeholder for fetching user data
-    const user: UserType = {
-      id: '1',
-      email: 'sample@example.com',
-      is_admin: true,
-      snippets: ['Snippet 1', 'Snippet 2']
-    }
-
-    setUser(user)
-    // Checks if user session exists 
-    /*
-    fetch('/api/authorized')
-      .then(res => {
-        if (res.ok) {
-          return res.json()
-        } else {
-          setUser(null)
-        }
-      })
-      .then((data: UserType) => {
-        setUser(data) 
-      })
-      .catch(error => {
-        console.error("Error fetching user:", error)
-        setUser(null)
-      })
-    */
-  }, [])
-
+  // need to update this with correct auth endpoint
+  // useEffect(() => {
+  //   // Checks if user session exists 
+  //   fetch('/api/authorized')
+  //     .then(res => {
+  //       if (res.ok) {
+  //         return res.json()
+  //       } else {
+  //         setUser(null)
+  //       }
+  //     })
+  //     .then((data: UserType) => {
+  //       setUser(data) 
+  //     })
+  //     .catch(error => {
+  //       console.error("Error fetching user:", error)
+  //       setUser(null)
+  //     })
+  // }, [])
 
   // Return the UserContext Provider with the user in value
   return (
