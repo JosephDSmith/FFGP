e23689499fe478297d4405fbcefa083d919105d5
Joseph Smith <joesmith40@gmail.com>
2023-09-08 16:36:02 -0400

    corrected config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,11 @@ import Home from './pages/Home/Home';
 import Languages from './pages/Languages/Languages';
 import Settings from './pages/Settings/Settings';
 import './App.css'
+import './tailwind.css'
+// comment in or out line 9 to see tailwind
+// to build the tailwind.css file, run the following command from the client folder:
+// npx tailwindcss build -o src/tailwind.css
+
 
 export default function App() {
 
