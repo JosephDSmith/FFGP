f2da105a44b8515e3ab08ce2c294e836811bb7a7
matthew rendely <matthew.rendely@gmail.com>
2023-09-12 19:49:56 -0700

    fix tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
 }
+
