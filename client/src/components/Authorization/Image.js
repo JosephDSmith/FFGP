f71810d4a0cf0058e4cef53d6b1ff0189b4bd3d2
Sofia <katzmansof@gmail.com>
2023-09-13 17:54:56 -0400

    create components for welcome card

---
+++ client/src/components/Authorization/Image.js
@@ -0,0 +1,5 @@
+const Image = ({ src, alt }) => {
+  return <img src={src} alt={alt} className="w-6 h-6 inline-block mr-2" />
+}
+
+export default Image
---
+++ client/src/components/Authorization/WelcomeMessage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const WelcomeMessage = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold mb-4">Welcome!</h2>
+      <p className="text-gray-600 text-sm mb-4">
+        We suggest you log in with your Google <br />account for full access to all features.
+      </p>
+    </div>
+  )
+}
+
+export default WelcomeMessage
