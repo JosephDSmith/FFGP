90f15a906248af4ebe0d6e6d6620458040aa89bc
Sofia <katzmansof@gmail.com>
2023-09-13 18:36:27 -0400

    add contribute and discover links

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,3 +1,4 @@
+// Navigation.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -5,8 +6,8 @@ interface NavigationProps {
   // Define any props being passed to Navigation component
 }
 
-// created a component to host styling of each link for readability 
-const NavLink: React.FC<{ to: string, children: string }> = ({ to, children }) => (
+// created a component to host styling of each link for readability
+const NavLink: React.FC<{ to: string; children: string }> = ({ to, children }) => (
   <Link
     to={to}
     className="text-black text-l hover:text-blue-500 transition-colors duration-300"
@@ -19,10 +20,10 @@ const Navigation: React.FC<NavigationProps> = () => {
   return (
     <div className="navigation text-center">
       <div className="flex justify-center space-x-20">
-        <NavLink to="/">auth</NavLink>
-        <NavLink to="/home">home</NavLink>
+        <NavLink to="/">auth</NavLink> {/* This will be replaced by a "login" button that will display only when the user is not logged in - logic pending */}
         <NavLink to="/languages">languages</NavLink>
-        <NavLink to="/settings">settings</NavLink>
+        <NavLink to="/discover">discover</NavLink>
+        <NavLink to="/contribute">contribute</NavLink>
       </div>
     </div>
   );
