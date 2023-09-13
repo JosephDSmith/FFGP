ce6414cba608cf2e7d65146439c4f33bb0e1228d
matthew rendely <matthew.rendely@gmail.com>
2023-09-12 19:10:14 -0700

    Wire up frontend to /api/tags and /api/snippets

--- client/src/pages/Languages/Languages.tsx
+++ client/src/pages/Languages/Languages.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {TagType} from '../../functionality/types';
 import './Languages.css'; 
 
 interface LanguagesProps {
@@ -6,10 +7,22 @@ interface LanguagesProps {
 }
 
 const Languages: React.FC<LanguagesProps> = (props) => {
+
+  const [tags, setTags] = useState<TagType[]>([]);
+
+  useEffect(() => {
+    fetch('/api/tags')
+    .then(r => r.json())
+    .then(d => setTags(d))
+
+  },[])
+  
   return (
     <div className="languages">
       {/* Languages content goes here */}
       <h1>THIS IS LANGUAGES </h1>
+      There are {tags.length} total tags 
+      {tags.map(t => (<div>{t.name}: {t.snippets.length} snippets</div>))}
     </div>
   );
 };
