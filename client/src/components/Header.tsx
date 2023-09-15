75fc0fd2c5951afc171d3421d1a81124ad181153
Sofia Katzman <katzmansof@gmail.com>
2023-09-15 17:56:26 -0400

    conditionally reroute user to home or auth from logo

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../functionality/UserContext';
+import { SnippetType } from '../functionality/types';
+
 
 interface HeaderProps {
     // Define any props being passed to Header component
 }
 
 const Header: React.FC = () => {
+  const { user } = useContext(UserContext) || { user: null };
+
   return (
     <header className="bg-gray-200 absolute top-0 left-0 right-0 p-4">
       <div className="container mx-auto">
         <h1 className="text-xl text-black text-right">
-          <Link to="/" className="hover:text-blue-500">
+          <Link to={user ? '/home' : '/'} className="hover:text-blue-500">
             [APP LOGO]
           </Link>
         </h1>
       </div>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
