4e7f53a4d911475bc98a4caf961e2f9ed2b55464
Sofia Katzman <katzmansof@gmail.com>
2023-09-15 00:16:12 -0400

    add sidebar and context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useContext } from 'react';
+import { UserContext } from './functionality/UserContext';
+import { UserType, UserContextType } from './functionality/types';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Authorization from './pages/Authorization';
@@ -7,18 +10,28 @@ import Languages from './pages/Languages';
 import './App.css'
 import Discover from './pages/Discover';
 import Contribute from './pages/Contribute';
+import UserSidebar from './components/UserSidebar/UserSidebar';
 // import './tailwind.css'
 // comment in or out line 9 to see tailwind
 // to build the tailwind.css file, run the following command from the client folder:
 // npx tailwindcss build -o src/tailwind.css
 
 export default function App() {
+  const { user } = useContext(UserContext) || { user: null }; 
+
   return (
     <div className="App">
       <Router>
         <Header />
+        {user && <UserSidebar user={user}/>}
         <div className="px-4 pt-24">
           <Routes>
+            {/* Render the Authorization component only if no user is logged in, otherwise render Home*/}
+              {/* {user === null ? (
+                <Route path="/" element={<Authorization />} />
+              ) : (
+                <Route path="/" element={<Home />} />
+              )} */}
             <Route path="/" element={<Authorization />} />
             <Route path="/home" element={<Home />} />
             <Route path="/languages" element={<Languages />} />
