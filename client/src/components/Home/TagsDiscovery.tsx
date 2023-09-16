1698bb21dde56149c64de3c9430d42b1aad5445e
Sofia Katzman <katzmansof@gmail.com>
2023-09-16 00:43:25 -0400

    create home page components

---
+++ client/src/components/Home/TagsDiscovery.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+interface TagsDiscoveryProps {
+
+}
+
+const TagsDiscovery: React.FC<TagsDiscoveryProps> = () => {
+  return (
+    <div className="text-center bg-blue-200 p-20 w-100% ">
+      <p className="text-base">
+      Dive into the world of coding and explore new programming languages. 
+      Our platform is a hub for not only finding solutions but also for broadening your coding horizons. 
+      Discover exciting languages, techniques, and approaches you might not have encountered otherwise. <br/>
+      Click on one of the tags below to find out more! 
+      </p>
+    {/* random tags logic here for discovery  */}
+    </div>
+  )
+}
+
+export default TagsDiscovery
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { SnippetType } from '../functionality/types';
+import CounterHeader from '../components/Home/CounterHeader';
+import UserDetails from '../components/Home/UserDetails';
+import ContributeBlurb from '../components/Home/ContributeBlurb';
+import TagsDiscovery from '../components/Home/TagsDiscovery';
 
 interface HomeProps {
   // Define any props being passed to Home component
@@ -8,17 +12,23 @@ interface HomeProps {
 const Home: React.FC<HomeProps> = (props) => {
   const [snippets, setSnippets] = useState<SnippetType[]>([]);
 
-  useEffect(() => {
+  useEffect( () => {
     fetch('/api/snippets')
       .then(r => r.json())
-      .then(d => setSnippets(d))
+      .then(d => {
+        setSnippets(d)
+      })
+  },[])
 
-  }, [])
   return (
+
     <div className="home">
+      <CounterHeader count={snippets.length} />
+      <UserDetails />
+      <ContributeBlurb />
+      <TagsDiscovery />
       {/* Home content goes here */}
-      <h1>THIS IS HOME </h1>
-      {snippets.map(s => (
+      {/* {snippets.map(s => (
         <div className='snippet'>
           <div>
             <b>Text content: </b>
@@ -34,7 +44,8 @@ const Home: React.FC<HomeProps> = (props) => {
             <b>Tags: </b>
             {s.tags?.map(t => t.name).join(', ')}
           </div>
-        </div>))}
+        </div>))} */}
+
     </div>
   );
 };
