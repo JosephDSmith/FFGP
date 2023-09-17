c84c5115e08a0bfbb7a95f4cdd7d10d903706268
Sofia Katzman <katzmansof@gmail.com>
2023-09-17 19:44:03 -0400

    remove unec. comments

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,14 +6,10 @@ import Footer from './components/Layout/Footer';
 import Authorization from './pages/Authorization';
 import Home from './pages/Home';
 import Languages from './pages/Languages';
-import './App.css'
 import Discover from './pages/Discover';
 import Contribute from './pages/Contribute';
 import UserSidebar from './components/UserSidebar/UserSidebar';
-// import './tailwind.css'
-// comment in or out line 9 to see tailwind
-// to build the tailwind.css file, run the following command from the client folder:
-// npx tailwindcss build -o src/tailwind.css
+import './App.css'
 
 export default function App() {
   const { user } = useContext(UserContext) || { user: null }; 
@@ -23,14 +19,8 @@ export default function App() {
       <Router>
         <Header />
         {user && <UserSidebar user={user}/>}
-        <div className="">
+        <div className="mt-4">
           <Routes>
-            {/* Render the Authorization component only if no user is logged in, otherwise render Home*/}
-              {/* {user === null ? (
-                <Route path="/" element={<Authorization />} />
-              ) : (
-                <Route path="/" element={<Home />} />
-              )} */}
             <Route path="/" element={<Authorization />} />
             <Route path="/home" element={<Home />} />
             <Route path="/languages" element={<Languages />} />
