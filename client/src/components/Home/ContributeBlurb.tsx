0e281172c8c6525c9b8e5ef45a8ae6f87348e653
Sofia Katzman <katzmansof@gmail.com>
2023-09-17 17:43:13 -0400

    add animated css arrow

--- client/src/components/Home/ContributeBlurb.tsx
+++ client/src/components/Home/ContributeBlurb.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Arrow.css'
 
-interface CounterHeaderProps {
-
-}
+interface CounterHeaderProps {}
 
 const ContributeBlurb: React.FC<CounterHeaderProps> = () => {
   return (
-    <div className="text-center bg-gray-100 p-20 w-100% ">
+    <div className="text-center bg-gray-100 p-20 w-100%">
       <p className="text-base m-1">
-      Are you ready to make a difference? 
-      Contribute your own code snippets and help shape our vibrant community. 
-      Whether you're a seasoned pro or just starting, your knowledge is valuable. 
-      Share your code to inspire, educate, and elevate the learning experience for all.
-      </p><br/>
-      <div className="m-10 p-4 max-w-sm text-gray-600 bg-white rounded-full flex justify-center items-center">
-        <Link to="/contribute">contribute</Link>
+        Are you ready to make a difference? Contribute your own code snippets
+        and help shape our vibrant community. Whether you're a seasoned pro or
+        just starting, your knowledge is valuable. Share your code to inspire,
+        educate, and elevate the learning experience for all.
+      </p>
+      <br />
+      {/* arrow to contribute */}
+      <Link to="/contribute" className="contribute-link hover:text-gray-500 transition-colors duration-300">
+        contribute
+      </Link>
+      <div className="flex justify-center items-center">
+        <div className="arrow arrow-first"> </div>
+        <div className="arrow arrow-second"></div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ContributeBlurb
+export default ContributeBlurb;
