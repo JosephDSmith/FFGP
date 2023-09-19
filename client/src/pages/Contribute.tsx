a3736aad9cbcd6a24fe2dc9707155bd4129a6c02
matthew rendely <matthew.rendely@gmail.com>
2023-09-18 20:10:46 -0700

    basic styling and import SelectedTags and TagList components

--- client/src/pages/Contribute.tsx
+++ client/src/pages/Contribute.tsx
@@ -1,18 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { Snippet } from "../functionality/types";
+import TagList from '../components/Discover/TagList';
+import SelectedTags from '../components/Discover/SelectedTags';
+import { TagType } from '../functionality/types';
 
-interface ContributeProps {}
+interface ContributeProps { }
 
-interface TagType {
-  id: number;
-  name: string;
-}
 
 const Contribute: React.FC<ContributeProps> = () => {
   const [textContent, setTextContent] = useState<string>("");
   const [image, setImage] = useState<File | null>(null);
   const [tags, setTags] = useState<TagType[]>([]);
-  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const [selectedTags, setSelectedTags] = useState<number[]>([]);
   const [newTag, setNewTag] = useState<string>("");
   const [showNewTagInput, setShowNewTagInput] = useState<boolean>(false);
 
@@ -30,8 +29,6 @@ const Contribute: React.FC<ContributeProps> = () => {
 
     fetchTags();
   }, []);
-
-  console.log(tags);
 
   const handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTextContent(event.target.value);
@@ -52,18 +49,19 @@ const Contribute: React.FC<ContributeProps> = () => {
       const newTagObject: TagType = {
         id: tags.length + 1, // You can generate an ID as needed
         name: newTag,
+        snippets: []
       };
       setTags([...tags, newTagObject]);
-      setSelectedTags([...selectedTags, newTag]);
+      setSelectedTags([...selectedTags, newTagObject.id]);
       setNewTag("");
     }
   };
 
-  const handleTagSelect = (selectedTag: string) => {
-    setSelectedTags([...selectedTags, selectedTag]);
-    setTags(tags.filter((tag) => tag.name !== selectedTag));
+  const handleTagSelect = (selectedTag: number) => {
+    setSelectedTags(selectedTags => [...selectedTags, selectedTag]);
+    setTags(tags.filter((tag) => tag.id !== selectedTag));
   };
-  
+
 
   const handleSubmit = () => {
     // Check if required fields are filled (text content or image)
@@ -106,49 +104,70 @@ const Contribute: React.FC<ContributeProps> = () => {
     //   });
   };
 
+  const selectedTagObjects = selectedTags
+    .map((tagId) => tags.find((tag) => tag.id === tagId))
+    .filter((tag) => tag !== undefined) as TagType[];
+
+  console.log(selectedTagObjects);
+
   return (
     <div>
-      <h2>Contribute a Snippet</h2>
-      <label>Enter Text Content:</label>
-      <input type="text" value={textContent} onChange={handleTextChange} />
-
-      <div>
-        <span>Or</span>
+      <div className='mx-auto max-w-2xl p-10'>
+        We invite you to become a valued contributor to our growing community. Share
+        your expertise, insights, and solutions with others by uploading your own
+        contributions.
       </div>
+      <div className='bg-white mb-20'>
+        <div className='mx-auto  max-w-2xl p-10'>
 
-      <label>Upload a Photo:</label>
-      <input type="file" accept="image/*" onChange={handleImageChange} />
+          <div className="flex-row  text-center">
+            <div className="text-xl m-5"> Contribute a snippet</div>
+            <input
+              type="text"
+              placeholder='Snippet text here'
+              value={textContent}
+              onChange={handleTextChange}
+              className="rounded-lg border-2 border-gray-300 text-center m-5  w-full h-20"
+            />
+            <div>or</div>
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleImageChange}
+              className="m-5"
+            />
+            <TagList tags={tags} selectedTags={selectedTags} onTagClick={handleTagSelect} />
+            <SelectedTags selectedTags={selectedTagObjects} />
 
-      <div>
-        <h3>Select Tags:</h3>
-        <ul>
-          {tags.map((tag) => (
-            <li key={tag.id} onClick={() => handleTagSelect(tag.name)}>
-              {tag.name}
-            </li>
-          ))}
-        </ul>
-        <div>
-          <input
-            type="text"
-            placeholder="Enter a new tag"
-            value={newTag}
-            onChange={handleTagChange}
-          />
-          <button onClick={handleTagSubmit}>Add Tag</button>
+
+            <div className='m-5'>
+              <input
+                type="text"
+                placeholder="Enter a new tag"
+                value={newTag}
+                onChange={handleTagChange}
+              />
+              <button
+                onClick={handleTagSubmit}
+                className='rounded-lg bg-blue-200 p-2 hover:bg-blue-400 '
+              >
+                Add Tag
+              </button>
+            </div>
+          </div>
+
+        </div>
+        <div className="text-center pb-10">
+        <button
+          className='rounded-md bg-blue-200 p-5 text-center'
+          onClick={handleSubmit}
+        >
+          Submit Snippet with Tag
+        </button>
         </div>
       </div>
 
-      <div>
-        <h3>Selected Tags:</h3>
-        <ul>
-          {selectedTags.map((tag, index) => (
-            <li key={index}>{tag}</li>
-          ))}
-        </ul>
-      </div>
 
-      <button onClick={handleSubmit}>Submit Snippet with Tag</button>
     </div>
   );
 };
