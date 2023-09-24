b7607588dd080137a2e6c601f91dfc644b0177b7
Sofia Katzman <katzmansof@gmail.com>
2023-09-24 03:10:29 -0400

    Add language tags on home page

--- client/src/components/Home/TagsDiscovery.tsx
+++ client/src/components/Home/TagsDiscovery.tsx
@@ -1,21 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { TagType } from '../../functionality/types';
 
-interface TagsDiscoveryProps {
-
-}
+interface TagsDiscoveryProps {}
 
 const TagsDiscovery: React.FC<TagsDiscoveryProps> = () => {
-  return (
-    <div className="text-center bg-white p-20 w-100% ">
-      <p className="text-base">
-      Dive into the world of coding and explore new programming languages. 
-      Our platform is a hub for not only finding solutions but also for broadening your coding horizons. 
-      Discover exciting languages, techniques, and approaches you might not have encountered otherwise. <br/><br/>
-      Click on one of the tags below to find out more! 
-      </p>
-    {/* random tags logic here for discovery  */}
-    </div>
-  )
-}
+  const [randomTags, setRandomTags] = useState<TagType[]>([]);
+  const [allTags, setAllTags] = useState<TagType[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
-export default TagsDiscovery
+  useEffect(() => {
+    // Fetch all tags from your API
+    fetch("/api/tags")
+      .then((r) => r.json())
+      .then((data) => {
+        setAllTags(data);
+        const randomIndices = generateRandomIndices(data.length, 3);
+        const selectedTags = randomIndices.map((index) => data[index]);
+        setRandomTags(selectedTags);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching tags:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  // Function to generate random unique indices within a range
+  function generateRandomIndices(maxRange: number, count: number) {
+    const indices: number[] = [];
+    while (indices.length < count) {
+      const randomIndex = Math.floor(Math.random() * maxRange);
+      if (!indices.includes(randomIndex)) {
+        indices.push(randomIndex);
+      }
+    }
+    return indices;
+  }
+
+  return (
+    <div className="text-center bg-white p-20 w-full">
+      <p className="text-base">
+        Dive into the world of coding and explore new programming languages. Our platform is a hub for not only finding
+        solutions but also for broadening your coding horizons. Discover exciting languages, techniques, and approaches
+        you might not have encountered otherwise. <br />
+        <br />
+        Click on one of the tags below to find out more!
+      </p>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="mt-4 flex flex-wrap justify-center"> {/* Center the buttons */}
+          {randomTags.map((tag, index) => (
+            <Link
+              key={index}
+              to={`/discover/${tag.id}`}
+              className="bg-gray-400 p-2 m-1 rounded-full text-center text-white"
+              style={{ minWidth: '100px' }}
+            >
+              {tag.name}
+            </Link>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default TagsDiscovery;
--- client/src/components/Home/UserDetails.tsx
+++ client/src/components/Home/UserDetails.tsx
@@ -1,38 +1,82 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../../functionality/UserContext';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
 interface UserDetailsProps {}
 
 const UserDetails: React.FC<UserDetailsProps> = () => {
-  const { user } = useContext(UserContext) || { user: null }
+  const { user } = useContext(UserContext) || { user: null };
+  const [topLanguages, setTopLanguages] = useState<{ id: number; language: string; count: number }[]>([]);
+
+  useEffect(() => {
+    if (user) {
+      fetch('/api/snippets')
+        .then((r) => r.json())
+        .then((data) => {
+          // Count occurrences of each tag's name
+          const tagCounts: Record<string, { id: number; count: number }> = {};
+          data.forEach((snippet: any) => {
+            if (snippet.user_id === user.id && snippet.tags) {
+              snippet.tags.forEach((tag: any) => {
+                const tagName = tag.name;
+                if (tagName in tagCounts) {
+                  tagCounts[tagName].count++;
+                } else {
+                  tagCounts[tagName] = { id: tag.id, count: 1 };
+                }
+              });
+            }
+          });
+
+          // Convert tagCounts object into an array of objects
+          const languagesArray = Object.entries(tagCounts).map(([language, { id, count }]) => ({
+            id,
+            language,
+            count,
+          }));
+
+          // Sort the languages by count in descending order
+          languagesArray.sort((a, b) => b.count - a.count);
+
+          // Get the top 3 languages
+          const top3Languages = languagesArray.slice(0, 3);
+
+          setTopLanguages(top3Languages);
+        })
+        .catch((error) => {
+          console.error('Error fetching snippets:', error);
+        });
+    }
+  }, [user]);
 
   return (
     <div className="user-details p-24 text-center bg-white m-w-m">
       <img
         src={user?.picture || 'https://t.ly/JM6x_'}
         alt="User Profile"
-        className="mx-auto w-24 h-24 rounded-full mb-2" // Adjust the w-24 and h-24 for size
+        className="mx-auto w-24 h-24 rounded-full mb-2" 
       />
       <div className="text-lg font-semibold">
         {user?.email || 'User Email'}
-        </div>
+      </div>
       <div className="flex justify-between max-w-sm m-auto gap-2">
-        <div className="text-sm text-gray-500 bg-gray-200 p-1 flex-1 text-left rounded-xl ">
-          contributions:
+        <div className="text-sm text-gray-500 bg-gray-200 p-1 flex-1 text-left rounded-xl">
+          Contributions:
         </div>
         <div className="text-sm text-gray-500 bg-gray-200 p-1 flex-1 text-right rounded-xl max-w-sm">
           10010
         </div>
       </div>
       <div className="top-languages pt-4">
-        
         <h2>Top Languages:</h2>
-        <ul>
-          {/* {sortedLanguages.map(([language, count]) => (
-            <li key={language}>
-              {language}: {count}
-            </li>
-          ))} */}
+        <ul className="flex justify-center items-center flex-wrap">
+          {topLanguages.map(({ id, language, count }) => (
+            <Link to={`/discover/${id}`} key={id}>
+              <li className="bg-gray-400 p-2 m-1 rounded-full text-center text-white w-48">
+                {language}: {count}
+              </li>
+            </Link>
+          ))}
         </ul>
       </div>
     </div>
