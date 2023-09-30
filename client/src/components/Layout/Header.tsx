09b0a77fcfcd021cb5b6bf403c47e4ae6c04e3ae
Sofia Katzman <katzmansof@gmail.com>
2023-09-30 02:50:36 -0400

    update spacing on temp logo placeholder

--- client/src/components/Layout/Header.tsx
+++ client/src/components/Layout/Header.tsx
@@ -10,12 +10,12 @@ const Header: React.FC<HeaderProps> = () => {
   const { user } = useContext(UserContext) || { user: null };
 
   return (
-    <header className="absolute top-0 left-0 right-0 py-4 items-end">
+    <header className="absolute top-0 left-0 right-0 py-4 items-end ">
       <div className="text-black flex justify-end">
         <Link to={user ? '/home' : '/'} className="hover:text-blue-500">
           <div className='flex items-center text-black mr-5'>
             {/* <img alt='logo' className='w-10 h-10 mr-2' src='/assets/logo/snippets_logo_small.png' /> */}
-            <span className="text-3 font-bold text-slate-500 pr-4">CODEWHISKER</span>
+            <span className="text-3 font-bold text-slate-500 pr-4">C O D E W H I S K E R</span>
           </div>
         </Link>
       </div>
